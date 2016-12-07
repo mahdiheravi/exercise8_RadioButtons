a8d9b84ff9996ac074fa8b93d0204137cb0033b3
hp-PC\hp <Mahdi.heravi@gmail.com>
2016-12-08 02:42:53 +0330

    Initialize java code!!! (create objects....)

--- app/src/main/java/com/example/hp/exercise8_radiobuttons/MainActivity.java
+++ app/src/main/java/com/example/hp/exercise8_radiobuttons/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.hp.exercise8_radiobuttons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -12,7 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
+        RadioButton rgbLondon = (RadioButton) findViewById(R.id.rdbLondon);
+        RadioButton rgbBeijing = (RadioButton) findViewById(R.id.rdbBlijng);
+        RadioButton rdbNewyork = (RadioButton) findViewById(R.id.rdbNewyork);
+
+        CheckBox chkTrans = (CheckBox) findViewById(R.id.chkTrsnsparent);
+        CheckBox chkVisible = (CheckBox) findViewById(R.id.chkVisibility);
+
+        ImageView IV = (ImageView) findViewById(R.id.IVpic);
+
+        Button btnset = (Button) findViewById(R.id.btnset);
 
 
 
