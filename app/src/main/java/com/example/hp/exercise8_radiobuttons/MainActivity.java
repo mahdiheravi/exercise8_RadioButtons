12eeb5d6aa32b2431f3ccb951d8bd69ea706f507
hp-PC\hp <Mahdi.heravi@gmail.com>
2016-12-08 03:39:30 +0330

    Set ImageView source programically !!!!!!

--- app/src/main/java/com/example/hp/exercise8_radiobuttons/MainActivity.java
+++ app/src/main/java/com/example/hp/exercise8_radiobuttons/MainActivity.java
@@ -2,29 +2,50 @@ package com.example.hp.exercise8_radiobuttons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioButton rgbLondon, rgbBeijing, rgbNewyork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RadioButton rgbLondon = (RadioButton) findViewById(R.id.rdbLondon);
-        RadioButton rgbBeijing = (RadioButton) findViewById(R.id.rdbBlijng);
-        RadioButton rdbNewyork = (RadioButton) findViewById(R.id.rdbNewyork);
+        rgbLondon = (RadioButton) findViewById(R.id.rdbLondon);
+        rgbBeijing = (RadioButton) findViewById(R.id.rdbBlijng);
+        rgbNewyork = (RadioButton) findViewById(R.id.rdbNewyork);
 
         CheckBox chkTrans = (CheckBox) findViewById(R.id.chkTrsnsparent);
         CheckBox chkVisible = (CheckBox) findViewById(R.id.chkVisibility);
 
-        ImageView IV = (ImageView) findViewById(R.id.IVpic);
+        final ImageView IV = (ImageView) findViewById(R.id.IVpic);
 
         Button btnset = (Button) findViewById(R.id.btnset);
+        btnset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (rgbLondon.isChecked()) {
 
+                    IV.setImageResource(getResources().getIdentifier("london","drawable",getPackageName()));
+                }
+                if (rgbBeijing.isChecked()) {
+                    IV.setImageResource(getResources().getIdentifier("beijing","drawable",getPackageName()));
+
+                }
+                if (rgbNewyork.isChecked()) {
+                    IV.setImageResource(getResources().getIdentifier("newyork","drawable",getPackageName()));
+
+                }
+
+            }
+        });
 
 
     }
